bfdf9ff4fccedf3dac6e6306717339600fd2578a
fnahuelc <fnahuelc@gmail.com>
2017-04-13 23:54:15 -0300

    algunos cambios

--- MercadoShark/managerApp/admin.py
+++ MercadoShark/managerApp/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import Item, MlUser, Globals
+from .models import Item, MlUser
 
 admin.site.register(Item)
 admin.site.register(MlUser)
-admin.site.register(Globals)
-
--- MercadoShark/managerApp/urls.py
+++ MercadoShark/managerApp/urls.py
@@ -6,15 +6,15 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     url(r'^create_item/$', views.create_item, name='create_item'),
-    url(r'^(?P<item_id>[0-9]+)/delete_items/$', views.delete_items, name='delete_items'),
+    url(r'^closed_item/(?P<item_id>[0-9]+)/$', views.closed_item, name='closed_item'),
+    url(r'^paused_item/(?P<item_id>[0-9]+)/$', views.paused_item, name='paused_item'),
+    url(r'^active_item/(?P<item_id>[0-9]+)/$', views.active_item, name='active_item'),
+    url(r'^delete_item/(?P<item_id>[0-9]+)/$', views.delete_item, name='delete_item'),
 
-    url(r'^create_testUser/$', views.create_testUser, name='create_testUser'),
-    url(r'^login_mlUser/$', views.login_mlUser, name='login_mlUser'),
-    url(r'^delete_account/(?P<account_id>[0-9]+)/$', views.delete_account, name='delete_account'),
-    url(r'^select_account/(?P<account_id>[0-9]+)$', views.select_account, name='select_account'),
+    url(r'^welcome/$', views.welcome, name='welcome'),
     url(r'^logout/$', views.logout, name='logout'),
 
-    url(r'^get_access_token$', views.get_access_token, name='get_access_token'),
+    url(r'^login', views.login, name='login'),
 
     url(r'^authorize_meli$', views.authorize_meli, name='authorize_meli'),
     url(r'^authorize_meli/(?P<code>w{0,150})$', views.authorize_meli, name='authorizing_meli')
